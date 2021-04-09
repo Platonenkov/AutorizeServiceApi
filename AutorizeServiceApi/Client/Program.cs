cf7527332794a0018a2f52c5d0ee40e5620603e2
APlatonenkov <platonenkov87@gmail.com>
2021-04-09 16:03:47 +0300

    Add views and provider

--- AutorizeServiceApi/Client/Program.cs
+++ AutorizeServiceApi/Client/Program.cs
@@ -7,6 +7,8 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using AutorizeServiceApi.Domain.Services;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.Extensions.Configuration.Memory;
 
 namespace AutorizeServiceApi.Client
@@ -18,9 +20,16 @@ namespace AutorizeServiceApi.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
+            builder.Services.AddOptions();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<CustomStateProvider>();
+            builder.Services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredService<CustomStateProvider>());
+            builder.Services.AddScoped<IAuthService, AuthService>();
+
+
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            var config = new Dictionary<string, string> { { "AuthorizeServiceAddress", "https://localhost:your_api_port/" } };
+            var config = new Dictionary<string, string> { { "AuthorizeServiceAddress", "http://localhost:60777" } };
             builder.Configuration.Add(new MemoryConfigurationSource { InitialData = config });
 
             await builder.Build().RunAsync();
